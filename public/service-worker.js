2882aadd2ec41954d673472718eaaa0379facfc7
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2021-01-04 14:49:56 -0700

    fix offline bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,21 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
+  '/',
   '/index.html',
   '/manifest.json',
-  './css/styles.css',
-  './js/idb.js',
-  './js/index.js'
-];
+  '/css/styles.css',
+  '/js/index.js',
+  '/js/idb.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+  '/api/transaction'];
 
 
 self.addEventListener('install', function (e) {
